e728b275762ecb8b03192e5c2a65d71ffcd47bcb
lezZen <yanezbruce123@gmail.com>
2023-03-10 12:16:30 -0500

    unncessary file

--- src/Sort&Filter/sortMenu.js
+++
@@ -1,8 +0,0 @@
-const closeMenu = () => {
-    sortMenu.classList.remove('sort-show');
-    sortMenu.classList.add('sort-hide');
-    setTimeout(() => {
-      sortMenu.classList.add('hidden');
-    }, 300);
-  };
-  
